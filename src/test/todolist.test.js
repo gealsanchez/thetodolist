06dbfdf194c69eef578641a58e5f21f5868d1d3a
gealsanchez <gealsanchez@gmail.com>
2023-01-26 10:19:55 -0500

    Testing implemented

--- src/test/todolist.test.js
+++ src/test/todolist.test.js
@@ -2,11 +2,10 @@ import Storage from '../module/classStorage.js';
 import Task from '../module/classTask.js';
 
 describe('Test methods', () => {
-test('add task to the list', () => {
+  test('add task to the list', () => {
     const task = new Task(1, 'tasking', false);
     Storage.addTask(task);
     const tasks = Storage.getTasks();
     expect(tasks.length).toBe(1);
   });
-
 });
