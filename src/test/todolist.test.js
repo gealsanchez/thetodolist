147695f515e71cebba8c576700f222358f017ae1
gealsanchez <gealsanchez@gmail.com>
2023-01-26 15:14:00 -0500

    Testing second stage

--- src/test/todolist.test.js
+++ src/test/todolist.test.js
@@ -16,7 +16,16 @@ describe('Test methods', () => {
     const tasks = Storage.getTasks();
     expect(tasks[0].description).toBe('driving');
   });
-  
+  test('edit completion from a task', () => {
+    Storage.editCompletion(1, true);
+    const tasks = Storage.getTasks();
+    expect(tasks[0].completed).toBe(true);
+  });
+  test('clear all checked tasks', () => {
+    Storage.clearAllCompleted(1, true);
+    const tasks = Storage.getTasks();
+    expect(tasks.length).toBe(0);
+  });
   test('delete task from the list', () => {
     Storage.removeTask(1);
     const tasks = Storage.getTasks();
